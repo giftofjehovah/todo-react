eb3b5fc461434e525eae1dfcf0c08b2964da54f7
Jonathan Phoon <jon@Jonathans-MacBook-Pro.local>
2017-02-06 11:44:50 +0800

    add build config

--- src/components/App.js
+++ src/components/App.js
@@ -2,5 +2,5 @@ import React from 'react'
 import { render } from 'react-dom'
 
 const root = document.querySelector('#root')
-const data = <h1>hi</h1>
+const data = <h1>helllo</h1>
 render(data, root)
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,28 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
 const config = {
   entry: './src/components/App.js',
   output: {
-    path: path.resolve(__dirname, 'src'),
+    path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
   },
   module: {
     rules: [
-      {test: /\.(js|jsx)$/, use: 'babel-loader'}
+      {test: /\.(js|jsx)$/, use: 'babel-loader'},
+      {test: /\.html$/, use: 'html-loader'}
     ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      hash: true
+    })
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, 'src'),
+    compress: true,
+    port: 8000
   }
 }
 module.exports = config
