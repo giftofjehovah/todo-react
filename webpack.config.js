4a0d80c07053534dc6887a178295beb05b4c705e
Jonathan Phoon <jonathan.pwh@gmail.com>
2017-02-14 11:52:13 +0800

    change webpack config for sourcemap

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 const config = {
   entry: './src/index.js',
-  devtool: 'eval-source-map',
+  devtool: 'source-map',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
