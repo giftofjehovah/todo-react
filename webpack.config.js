dd985aa0804a35c6a33302302d513a55241acff6
Jonathan Phoon <jonathan.pwh@gmail.com>
2017-02-20 18:33:05 +0800

    add fetch polyfill

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 const config = {
-  entry: './src/index.js',
+  entry: ['whatwg-fetch', './src/index.js'],
   devtool: 'source-map',
   output: {
     path: path.resolve(__dirname, 'dist'),
