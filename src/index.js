57efedcda64972c47d8d5cdc440e3aa65884eb14
Jonathan Phoon <jonathan.pwh@gmail.com>
2017-02-21 14:21:13 +0800

    add redux devtools

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import { Provider } from 'react-redux'
 import { createEpicMiddleware } from 'redux-observable'
 import reducer from './reducers'
@@ -9,8 +9,10 @@ import App from './containers/App'
 import 'todomvc-app-css/index.css'
 import './styles.css'
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
 const epicMiddleware = createEpicMiddleware(epic)
-const store = createStore(reducer, applyMiddleware(epicMiddleware))
+const store = createStore(reducer, composeEnhancers(applyMiddleware(epicMiddleware)))
 const app = document.querySelector('#app')
 store.subscribe(() => {
   console.log('state is changing', store.getState())
