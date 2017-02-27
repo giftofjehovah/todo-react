53e94aba1cec5a52e81ec7dd7e55324971c48fcb
Jonathan Phoon <jonathan.pwh@gmail.com>
2017-02-27 10:46:42 +0800

    add quote reducer spec

---
+++ __tests__/reducers/quote.spec.js
@@ -0,0 +1,15 @@
+/* global describe it expect */
+import quote from '../../src/reducers/quote'
+import { UPDATE_QUOTE_TO_STATE } from '../../src/constants/actionTypes'
+
+describe('quote reducers', () => {
+  it('should handle UPDATE_QUOTE_TO_STATE', () => {
+    const currentState = ''
+    const action = {
+      type: UPDATE_QUOTE_TO_STATE,
+      quote: 'I am your father'
+    }
+    const expectedResult = 'I am your father'
+    expect(quote(currentState, action)).toEqual(expectedResult)
+  })
+})
